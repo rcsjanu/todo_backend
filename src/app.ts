1a16f389f28827ae11924b4ce5d4ff6e3ea25521
Janki Patel <janu1573@gmail.com>
2025-05-23 21:06:53 +0530

    Fix errors and added globar error handler and security for application

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 import userAuthRoutes from './routes/userAuth.routes';
 import todoRoutes from './routes/todo.routes';
 import { scheduleUpdateOfTodoStatus } from './jobs/updateTodoStatusJob';
+import { errorHandler } from './middlewares/errorhandler.middleware';
 
 dotenv.config();
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}));
+
+app.use(errorHandler);
+
 app.use('/api/user-auth', userAuthRoutes);
 app.use('/api/todos', todoRoutes);
 
--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -20,13 +20,16 @@ export const updateTodo = async (req: AuthRequest, res: Response): Promise<void>
         req.body,
         { new: true }
     );
-    if (!updated) res.status(404).json({ message: 'Todo not found' });
+    if (!updated) {
+        res.status(404).json({ message: 'Todo not found' });
+        return;
+    }
     res.status(200).json({ message: "TODO updaated..!", data: updated });
 };
 
 export const deleteTodo = async (req: AuthRequest, res: Response): Promise<void> => {
     const { id } = req.params;
     const deleted = await Todo.findOneAndDelete({ _id: id, user: req.userId });
-    if (!deleted) res.status(404).json({ message: 'Todo not found' });
+    if (!deleted) { res.status(404).json({ message: 'Todo not found' }); return; }
     res.status(200).json({ message: 'TODO Deleted successfully..!' });
 };
